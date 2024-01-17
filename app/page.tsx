a93282e9de54bda75331e9205d77b3d3d19fd582
raspatimahatma <tommymahatma08@gmail.com>
2024-01-17 19:58:49 +0700

    just trying stuff

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-b from-zinc-200 dark:bg-zinc-800/30">
       <p className="text-3xl font-semibold text-center mb-8">
-        Hello, my name is Raspati Mahatma Kurnia Dharmatmaja
+        Hello There! 👋
       </p>
       <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
        
