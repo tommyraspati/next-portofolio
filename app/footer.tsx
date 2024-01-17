9777d650644dff773721e017912f795843f25758
raspatimahatma <tommymahatma08@gmail.com>
2024-01-17 22:30:32 +0700

    edit icon on footer to be responsive

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,33 +1,38 @@
-// Footer.tsx
-
+"use client";
+import { useState } from 'react';
 import { FaLinkedin, FaGithub, FaInstagram, FaEnvelope } from 'react-icons/fa';
 
 const Footer = () => {
- return (
-   <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-     <div className="flex gap-4">
-       {/* LinkedIn */}
-       <a href="https://www.linkedin.com/in/tommyraspati" target="_blank" rel="noopener noreferrer">
-         <FaLinkedin className="h-6 w-6 " />
-       </a>
-       
-       {/* GitHub */}
-       <a href="https://github.com/tommyraspati" target="_blank" rel="noopener noreferrer">
-         <FaGithub className="h-6 w-6" />
-       </a>
+  const [linkedinClicked, setLinkedinClicked] = useState(false);
+  const [githubClicked, setGithubClicked] = useState(false);
+  const [gmailClicked, setGmailClicked] = useState(false);
+  const [instagramClicked, setInstagramClicked] = useState(false);
 
-       {/* Gmail */}
-       <a href="mailto:tommyraspati@gmail.com">
-         <FaEnvelope className="h-6 w-6 " />
-       </a>
+  return (
+    <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+      <div className="flex gap-4">
+        {/* LinkedIn */}
+        <a href="https://www.linkedin.com/in/tommyraspati" target="_blank" rel="noopener noreferrer" onClick={() => setLinkedinClicked(!linkedinClicked)}>
+          <FaLinkedin className={`h-6 w-6 hover:scale-110 transition-transform ${linkedinClicked ? 'text-blue-600' : ''}`} />
+        </a>
+        
+        {/* GitHub */}
+        <a href="https://github.com/tommyraspati" target="_blank" rel="noopener noreferrer" onClick={() => setGithubClicked(!githubClicked)}>
+          <FaGithub className={`h-6 w-6 hover:scale-110 transition-transform ${githubClicked ? 'text-gray-800' : ''}`} />
+        </a>
 
-       {/* Instagram */}
-       <a href="https://www.instagram.com/tommyraspati" target="_blank" rel="noopener noreferrer">
-         <FaInstagram className="h-6 w-6 " />
-       </a>
-     </div>
-   </div>
- );
+        {/* Gmail */}
+        <a href="mailto:tommyraspati@gmail.com" onClick={() => setGmailClicked(!gmailClicked)}>
+          <FaEnvelope className={`h-6 w-6 hover:scale-110 transition-transform ${gmailClicked ? 'text-red-600' : ''}`} />
+        </a>
+
+        {/* Instagram */}
+        <a href="https://www.instagram.com/tommyraspati" target="_blank" rel="noopener noreferrer" onClick={() => setInstagramClicked(!instagramClicked)}>
+          <FaInstagram className={`h-6 w-6 hover:scale-110 transition-transform ${instagramClicked ? 'text-pink-600' : ''}`} />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
