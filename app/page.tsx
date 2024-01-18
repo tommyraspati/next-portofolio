c5bc6a0d3cb60c3278d5c44a984de6c0d24e32dc
raspatimahatma <tommymahatma08@gmail.com>
2024-01-18 23:48:41 +0700

    fix animation name bug

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,20 @@ export default function Home() {
       <h1 className={`${inter.className} text-1xl sm:text-2xl text-yellow-600 mb-2 sm:mb-4`}>
         Hello, My name is
       </h1>
-      <h1 className={`${lusitana.className} text-3xl sm:text-5xl text-black mb-2 sm:mb-4`}>
-        <Typewriter
-          options={{
-            strings: ['Raspati Mahatma Kurnia D.'],
-            autoStart: true,
-            loop: true,
-            delay: 50,
-          }}
-        />
-      </h1>
-      <h2 className={`${lusitana.className} text-1xl sm:text-2xl text-black mt-4 sm:mt-8 mb-2 sm:mb-4`}>
+      <div className="h-12 sm:h-24 "> {/* Set a fixed height for the typewriter container */}
+        <h1 className={`${lusitana.className} text-3xl sm:text-5xl text-black mb-2 sm:mb-4`}>
+          <Typewriter
+            options={{
+              strings: ['Raspati Mahatma Kurnia D.'],
+              autoStart: true,
+              loop: false,
+              deleteSpeed: Infinity, // Prevents deletion, can be omitted if not needed
+              delay: 50,
+            }}
+          />
+        </h1>
+      </div>
+      <h2 className={`${lusitana.className} text-1xl sm:text-2xl text-black mt-8 sm:mt-4 mb-2 sm:mb-4`}>
         I am currently a third-year Computer Science student at <HighlightText>Universitas Indonesia</HighlightText>, 
         where I am deeply engaged in exploring the realms of <HighlightText>Machine Learning</HighlightText>, <HighlightText>Data Science</HighlightText>, and <HighlightText>Software Engineering</HighlightText>.  
         I am eager to meet new people and embrace new challenges. My goal is to not only expand my technical 
