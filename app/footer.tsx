e275303463793ed4364902eca5933baf5aa79f89
raspatimahatma <tommymahatma08@gmail.com>
2024-01-18 17:48:24 +0700

    edit name animation

--- app/footer.tsx
+++ app/footer.tsx
@@ -7,36 +7,37 @@ const Footer = () => {
 
   const handleClick = (iconName: string) => {
     setActiveIcon(iconName);
+    // Reset active icon after 2 seconds
     setTimeout(() => {
       setActiveIcon('');
-    }, 2000); // 2000 milliseconds = 2 seconds
+    }, 2000);
   };
 
   return (
-    <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <div className="fixed inset-x-0 bottom-0 flex h-16 items-center justify-center dark:bg-gray-200">
       <div className="flex gap-4">
         {/* LinkedIn */}
         <a href="https://www.linkedin.com/in/tommyraspati" target="_blank" rel="noopener noreferrer"
            onClick={() => handleClick('linkedin')}>
-          <FaLinkedin className={`h-6 w-6 hover:scale-110 transition-transform ${activeIcon === 'linkedin' ? 'text-blue-600' : ''}`} />
+          <FaLinkedin className={`h-6 w-6 hover:scale-110 transition-transform ${activeIcon === 'linkedin' ? 'text-blue-600' : 'text-black'}`} />
         </a>
         
         {/* GitHub */}
         <a href="https://github.com/tommyraspati" target="_blank" rel="noopener noreferrer"
            onClick={() => handleClick('github')}>
-          <FaGithub className={`h-6 w-6 hover:scale-110 transition-transform ${activeIcon === 'github' ? 'text-gray-800' : ''}`} />
+          <FaGithub className={`h-6 w-6 hover:scale-110 transition-transform ${activeIcon === 'github' ? 'text-gray-800' : 'text-black'}`} />
         </a>
 
         {/* Gmail */}
         <a href="mailto:tommyraspati@gmail.com"
            onClick={() => handleClick('gmail')}>
-          <FaEnvelope className={`h-6 w-6 hover:scale-110 transition-transform ${activeIcon === 'gmail' ? 'text-red-600' : ''}`} />
+          <FaEnvelope className={`h-6 w-6 hover:scale-110 transition-transform ${activeIcon === 'gmail' ? 'text-red-600' : 'text-black'}`} />
         </a>
 
         {/* Instagram */}
         <a href="https://www.instagram.com/tommyraspati" target="_blank" rel="noopener noreferrer"
            onClick={() => handleClick('instagram')}>
-          <FaInstagram className={`h-6 w-6 hover:scale-110 transition-transform ${activeIcon === 'instagram' ? 'text-pink-600' : ''}`} />
+          <FaInstagram className={`h-6 w-6 hover:scale-110 transition-transform ${activeIcon === 'instagram' ? 'text-pink-600' : 'text-black'}`} />
         </a>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
-import Image from 'next/image';
-import { FaLinkedin, FaGithub, FaInstagram, FaEnvelope } from 'react-icons/fa';
+'use client';
 import Footer from './footer'; // Import Footer component
+import { lusitana } from '@/app/ui/fonts';
+import Typewriter from 'typewriter-effect';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-b from-zinc-200 dark:bg-zinc-800/30">
-      <p className="text-3xl font-semibold text-center mb-8">
-        Hello There! 👋
+ return (
+   <main className="flex min-h-screen flex-col items-start justify-start p-12 sm:p-24 bg-gray-50 dark:bg-gray-200">
+    <p className={`${lusitana.className} text-1xl sm:text-2xl text-yellow-600 mb-2 sm:mb-4`} >
+         Hello, My name is
+     </p>
+     <p className={`${lusitana.className} text-3xl sm:text-5xl text-black mb-4`}>
+      <Typewriter
+        options={{
+          strings: ['Raspati Mahatma Kurnia Dharmatmaja.'],
+          autoStart: true,
+          loop: true,
+          delay: 50,
+        }}
+      />
       </p>
-      
-      {/* Footer */}
-      <Footer /> {/* Use Footer component */}
-    </main>
-  )
+     {/* Footer */}
+     <Footer /> {/* Use Footer component */}
+   </main>
+ )
 }
